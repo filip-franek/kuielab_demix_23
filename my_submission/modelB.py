e60d3835c41488d43e3a7de0eeb126c7244b8e59
Filip Franek <filip@audiblebits.com>
2023-07-30 16:09:45 -0600

    Enable running inference on CPU

--- my_submission/modelB.py
+++ my_submission/modelB.py
@@ -13,17 +13,21 @@ ckpts = ['step150000_seed0', 'step150000_seed1', 'step150000_seed2']
 model_path = f'my_submission/ckpts/{model_name}'
 config_path = model_path+'/config.yaml'
 
-device = torch.device('cuda:0')
-
 
 class MusicSeparationModel:
     
-    def __init__(self):     
-        
+    def __init__(self):  
+
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+
         with open(config_path) as f:
             self.config = ConfigDict(yaml.load(f, Loader=yaml.FullLoader))
        
-        self.model = TFC_TDF_net(self.config).eval().cuda()
+        self.model = TFC_TDF_net(self.config).eval()
+        self.model = self.model.to(self.device)
                
     @property
     def instruments(self):
@@ -35,12 +39,12 @@ class MusicSeparationModel:
         raise NameError(msg)
     
     def separate_music_file(self, mixed_sound_array, sample_rate):
-        
-        mixture = torch.tensor(mixed_sound_array.T, dtype=torch.float32)
+
+        mixture = torch.tensor(mixed_sound_array.T, dtype=torch.float32).to(self.device)
         source_dicts = []
         for ckpt in ckpts:
             self.model.load_state_dict(
-                torch.load(model_path+f'/{ckpt}.ckpt')
+                torch.load(model_path+f'/{ckpt}.ckpt', map_location=torch.device('cpu'))
             )
             source_dicts.append(self.demix(mixture))
         
@@ -66,7 +70,7 @@ class MusicSeparationModel:
         L = mix.shape[1]    
         pad_size = H-(L-C)%H
         mix = torch.cat([torch.zeros(2,C-H), mix, torch.zeros(2,pad_size + C-H)], 1)
-        mix = mix.cuda()
+        mix = mix.to(self.device)
 
         chunks = []
         i = 0
@@ -81,17 +85,15 @@ class MusicSeparationModel:
             batches.append(chunks[i:i+batch_size])
             i = i + batch_size
 
-        X = torch.zeros(len(self.instruments),2,C-H)
-        X = X.cuda()
-        with torch.cuda.amp.autocast():
-            with torch.no_grad():
-                for batch in batches:
-                    x = self.model(batch) 
-                    for w in x:
-                        a = X[...,:-(C-H)]
-                        b = X[...,-(C-H):] + w[...,:(C-H)]
-                        c = w[...,(C-H):]
-                        X = torch.cat([a,b,c], -1)
+        X = torch.zeros(len(self.instruments),2,C-H, device=self.device)
+        with torch.no_grad():
+            for batch in batches:
+                x = self.model(batch) 
+                for w in x:
+                    a = X[...,:-(C-H)]
+                    b = X[...,-(C-H):] + w[...,:(C-H)]
+                    c = w[...,(C-H):]
+                    X = torch.cat([a,b,c], -1)
 
         estimated_sources = X[..., C-H:-(pad_size+C-H)]/N
         
